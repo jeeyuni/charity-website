4c70b7d1a4c371804346cee494f58e0dcefea03b
Jiyun Lee <kuri12071112@gmail.com>
2024-10-16 00:46:16 +0700

    connected api

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+    // api
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("https://partners.every.org/v0.2/search/pets?apiKey=pk_live_8483ffb3c4f743b7e505838df7349127")
+            .then(response => response.json())
+            .then(json => setData(json))
+            .catch(error => console.error(error));
+    }, []);
+
+
+
     return (
         <div className='App'>
             <Header />
@@ -22,6 +35,11 @@ function App() {
                     element={<CampsiteDetailPage />}
                 />
             </Routes>
+
+            {/* testing api output */}
+            <div>
+                {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+            </div>
             <Footer />
         </div>
     );
